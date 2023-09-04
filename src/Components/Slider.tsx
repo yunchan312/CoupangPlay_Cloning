817ab4bc183515dfda5118882eade36374998a79
yunchan312 <phenomenal312@hufs.ac.kr>
2023-09-04 23:36:22 +0900

    slider image call

--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -1,21 +1,52 @@
 import { IGetMoviesResult, getPopularMovies } from "../api";
 import styled from "styled-components";
 import { useQuery } from "react-query";
+import { useState } from "react";
+import { makeImagePath } from "../Router/util";
 
-const Row = styled.div``;
+const Row = styled.div`
+  display: flex;
+  position: relative;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+`;
 
-const Box = styled.div``;
+const Container = styled.div``;
+
+const Loader = styled.div``;
+
+const Box = styled.div<{ bgphoto: string }>`
+  background-color: white;
+  width: 300px;
+  height: 200px;
+  background-image: url(${(props) => props.bgphoto});
+  background-position: center center;
+  background-size: cover;
+`;
+
+const offset = 6;
 
 export default function Slider() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "popular"],
     getPopularMovies
   );
+  const listLn = data?.results.length;
+  const [index, setIndex] = useState(0);
   return (
     <>
-      <Row>
-        <Box></Box>
-      </Row>
+      <Container>
+        {isLoading ? (
+          <Loader>loading</Loader>
+        ) : (
+          <Row>
+            {data?.results.slice(0, 6).map((movie) => (
+              <Box bgphoto={makeImagePath(movie.backdrop_path, "w500")}></Box>
+            ))}
+          </Row>
+        )}
+      </Container>
     </>
   );
 }
